1fc80fcb1b77502d94771bf9b405bc248f1138c5
vuthlang <103568613+vuthlang@users.noreply.github.com>
2023-05-31 16:05:06 +0200

    initial commit

---
+++ website/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Materiel(models.Model):
+    materiel = models.ForeignKey(Product, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
