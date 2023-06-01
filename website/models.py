75172440d531d49c3d0d7a4272aef3f0a95d154f
vuthlang <103568613+vuthlang@users.noreply.github.com>
2023-06-01 13:55:49 +0200

    table produit

--- website/models.py
+++ website/models.py
@@ -3,16 +3,13 @@ from django.db import models
 
 class Product(models.Model):
     product_name = models.CharField(max_length=200)
+    price = models.DecimalField(
+        max_digits=5, 
+        decimal_places=2,
+        default=0
+    )
+    stock = models.IntegerField(default=0)
     pub_date = models.DateTimeField("date published")
-
-    def __str__(self):
-        return self.product_name
-
-
-class Materiel(models.Model):
-    materiel = models.ForeignKey(Product, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.product_name
